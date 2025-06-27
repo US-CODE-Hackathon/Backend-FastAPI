7c3a5872b4ee5451964a9694a7b31ed20e0c5d73
NoteAnywhere <noteanywhere@example.com>
2025-06-28 00:13:55 +0900

    feat: summary, sentiment 라우터를 FastAPI 앱에 등록

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
+from summary.router import router as summary_router
+from sentiment.router import router as sentiment_router
 
 app = FastAPI()
 
@@ -13,3 +15,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+app.include_router(summary_router)
+app.include_router(sentiment_router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
