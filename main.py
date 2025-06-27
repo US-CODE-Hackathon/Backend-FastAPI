0d69b77244864eb0293612c001d12a3dffbc0e7d
NoteAnywhere <noteanywhere@example.com>
2025-06-28 03:13:16 +0900

    feat: 불필요 코드 삭제

--- main.py
+++ main.py
@@ -2,23 +2,15 @@ from typing import Union
 
 from fastapi import FastAPI
 from summary.router import router as summary_router
-from sentiment.router import router as sentiment_router
 
 app = FastAPI()
+app.include_router(summary_router)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-app.include_router(summary_router)
-app.include_router(sentiment_router)
 
 if __name__ == "__main__":
     import uvicorn
