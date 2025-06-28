bed84377c0d2b8bc59000ca13746f1d77da64a81
NoteAnywhere <noteanywhere@example.com>
2025-06-28 16:00:46 +0900

    feat: EmotionalReport 모델을 emotional_report 테이블 구조에 맞게 정의

--- summary/model.py
+++ summary/model.py
@@ -1,12 +1,14 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
 from db import Base
 
 
-class Summary(Base):
-    __tablename__ = "summary"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(255))
+class EmotionalReport(Base):
+    __tablename__ = "emotional_report"
+    emotional_report_id = Column(Integer, primary_key=True, index=True)
+    is_first = Column(Boolean)
+    conversation_id = Column(Integer)
+    created_at = Column(DateTime)
+    image_url = Column(Text)
     sentiment = Column(String(50))
     summary = Column(Text)
-    photo = Column(Text)
-    date = Column(DateTime)
+    title = Column(String(255))
