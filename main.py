ce4a3fe3be51f7070ae540a07aba2991c59eddaa
NoteAnywhere <noteanywhere@example.com>
2025-06-28 13:31:41 +0900

    feat: cors set

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from summary.router import router as summary_router
 from db import SessionLocal, engine, Base
 from stt_tts.router import router as stt_tts_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "https://us-hackathon-3db73.firebaseapp.com",
+    "https://us-hackathon-3db73.web.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(summary_router)
 app.include_router(stt_tts_router)
 
